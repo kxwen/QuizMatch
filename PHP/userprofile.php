5c07f3cab05976740d7c0ee2ce16a0436c181e67
ZatchZ <43761976+ZatchZ@users.noreply.github.com>
2018-11-14 08:05:21 -0800

    Buttons now spill over to next line.

--- PHP/userprofile.php
+++ PHP/userprofile.php
@@ -40,19 +40,23 @@ Hovering over the card QuizMatch will produce one of many random anecdotes.
 		</style>
 	</head>
 	<body>
-		<center><h2>Welcome, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h2></center>
-		<br>
-		<center><img src="images/default-user2.png" alt="Default User Profile" width="15%" height="15%"></center>
-		<br>
-		<div class = "logoCenter">
-			<a href="login.php" class="btn large white rounded"><tt>My Profile<i class="material-icons">person</i></tt></a> 	
-		</div>
-
-		<div class = "buttonCenter">
-			<a class="btn large pink rounded"><tt>Matches&#x1F50D;</tt></a> 	
-			<a class="btn large pink rounded"><tt>Messages&#9993;</tt></a>
-			<a href="quiz_home.php" class="btn large pink rounded"><tt>Create a Quiz!&#10004 </tt></a>
-			<a href="logout.php" class="btn large pink rounded"><tt>Logout <i class="fa fa-sign-out"></i></tt></a>
+		<div class = "container">
+			<center><h2>Welcome, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h2></center>
+			<br>
+			<center><img src="images/default-user2.png" alt="Default User Profile" width="15%" height="15%"></center>
+			<br>
+			<div class = "logoCenter">
+				<a href="login.php" class="btn large white rounded"><tt>My Profile<i class="material-icons">person</i></tt></a> 	
+			</div>
+				<div class = "buttonCenter">
+					<div class = "container">
+						<a class="btn large pink rounded"><tt>Matches&#x1F50D;</tt></a> 	
+						<a class="btn large pink rounded"><tt>Messages&#9993;</tt></a>
+						<a href="quiz_home.php" class="btn large pink rounded"><tt>Quizzes!&#10004 </tt></a>
+						<a href="logout.php" class="btn large pink rounded"><tt>Logout <i class="fa fa-sign-out"></i></tt></a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
