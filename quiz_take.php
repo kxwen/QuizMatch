a7cd9df075e664c59fd153f7a13b48f1427f9965
anthonychian <anthonychian@gmail.com>
2018-11-29 17:41:32 -0800

    Add files via upload

--- quiz_take.php
+++ quiz_take.php
@@ -26,6 +26,15 @@ $current_name = $quiz["name"];
 $current_desc = $quiz["description"];
 $current_size = $quiz["size"];
 
+$num_questions = 0;
+
+if ($current_size == "small")
+	$num_questions = 5;
+else if ($current_size == "medium")
+	$num_questions = 10;
+else if ($current_size == "large")
+	$num_questions = 15;
+
 // Array containing each question from specified quiz
 $quiz_questions = getQuizQuestions($link, $current_quiz);
 
@@ -34,7 +43,7 @@ $quiz_answers = array();
 
 // For each question, retrieve set of answers for specified quiz id
 // quiz
-for ($i = 0; $i < 20; $i++) {
+for ($i = 0; $i < $num_questions; $i++) {
 
 	if(!isset($question)){
 		$sql = "SELECT * FROM questions WHERE quiz_id = ".$current_quiz;
@@ -51,80 +60,100 @@ for ($i = 0; $i < 20; $i++) {
 ?>
 
 
-<html>
+<!DOCTYPE html>
 <head>
 	<title>Quiz Match</title>
-	<!-- <link href="stupid.css" type="text/css" rel="stylesheet" /> -->
-	<style>
-	.form {
-	font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
-	font-size: 0.8em;
-	width: 55em;
-	padding: 2em;
-	border: 2px solid #ccc;
-	}
-
-	.form fieldset {
-	border: none;
-	padding-left: 20;
-	}
-
-	.form legend,
-	.form label {
-	padding: 5;
-	font-weight: bold;
-	}
-
-	.form label.choice {
-	font-size: 0.9em;
-	font-weight: normal;
-	}
-
-	.form button {
-	padding: 1em;
-	border-radius: 0.5em;
-	background: #eee;
-	border: none;
-	font-weight: bold;
-	margin-top: 1em;
-	}
-
-	</style>
+	<link href="stupid.css" type="text/css" rel="stylesheet" />
 </head>
+
 <body>
 	<center>
-	<h2> <?php echo "Quiz: " . $current_name;?> </h2>
-	<h4> <?php echo $current_desc;?> </h4>
-	<form class="form" method="POST" enctype="application/x-www-form-urlencoded" action="questions_result.php" name="quiz">
 
-	<fieldset>
+	<div class="contentRoundBorders" id="quizForm">
+		<form class="form" method="POST" enctype="application/x-www-form-urlencoded" action="questions_result.php" name="quiz">
+			<h2> <?php echo "Quiz: " . $current_name;?> </h2>
+			<h4> <?php echo $current_desc;?> </h4><br><br>
+			<span id="Quiz_Pages"></span>
 
-	<legend> <?php echo $quiz_questions[0]["question"];?> </legend>
-	<p><label class="choice"> <input type="radio" name="r1" value="A"> <?php echo $quiz_answers[0][0]["answer"];?> </label></p>
-	<p><label class="choice"> <input type="radio" name="r1" value="B"> <?php echo $quiz_answers[0][1]["answer"];?> </label></p><br><br>
+			<button type="button" class ="btn pink rounded" id="prevBtn"
+				onclick="nextPrev(-1);">Previous</button>
+			<button type="button" class ="btn pink rounded" id="nextBtn"
+				onclick="nextPrev(1);">Next</button>
+			<div style="text-align:center;margin-top:40px;">
+				<span id="Quiz_Page_Steps"></span>
+			</div>
+		</form>
 
-	<legend> <?php echo $quiz_questions[1]["question"];?> </legend>
-	<p><label class="choice"> <input type="radio" name="r1" value="A"> <?php echo $quiz_answers[1][0]["answer"];?> </label></p>
-	<p><label class="choice"> <input type="radio" name="r1" value="B"> <?php echo $quiz_answers[1][1]["answer"];?> </label></p><br><br>
+	</div>	
+	<script src="config.js"></script>
+	<script>
+			var currentTab = 0; // Current tab is set to be the first tab (0)
+			var max_answers = 4;
+			var total_questions =  <?=$num_questions?>;
+			var questions = [];
+			questions = <?= json_encode($quiz_questions)?>;
+			var answers = [];
+			answers = <?= json_encode($quiz_answers)?>;
 
-	<legend> <?php echo $quiz_questions[2]["question"];?> </legend>
-	<p><label class="choice"> <input type="radio" name="r1" value="A"> <?php echo $quiz_answers[2][0]["answer"];?> </label></p>
-	<p><label class="choice"> <input type="radio" name="r1" value="B"> <?php echo $quiz_answers[2][1]["answer"];?> </label></p><br><br>
+			
 
-	<legend> <?php echo $quiz_questions[3]["question"];?> </legend>
-	<p><label class="choice"> <input type="radio" name="r1" value="A"> <?php echo $quiz_answers[3][0]["answer"];?> </label></p>
-	<p><label class="choice"> <input type="radio" name="r1" value="B"> <?php echo $quiz_answers[3][1]["answer"];?> </label></p><br><br>
+			displayQuestionsByTab(currentTab, questions, answers);
 
-	<legend> <?php echo $quiz_questions[4]["question"];?> </legend>
-	<p><label class="choice"> <input type="radio" name="r1" value="A"> <?php echo $quiz_answers[4][0]["answer"];?> </label></p>
-	<p><label class="choice"> <input type="radio" name="r1" value="B"> <?php echo $quiz_answers[4][1]["answer"];?> </label></p>
+			function displayQuestionsByTab(currentTab, questions, answers) {
+				createQuestionPages(currentTab, questions, answers);
+				showTab(currentTab);
+
+			}
+
+			function createQuestion() {
+				var question_tab;
+				var question_step;
+				for(var i =0;i<total_questions; i++){
+					question_tab = document.createElement("DIV");
+					var question_content;
+				
+					question_content = document.createTextNode(questions[i]["question"]);
+					question_tab.appendChild(question_content);
+					question_tab.innerHTML += "<br>";
+
+					for (var j = 0; j < answers[i].length; j++) {
+
+						var ans_content = document.createTextNode(" "+answers[i][j]["answer"]);
+						var radio = document.createElement("input");
+
+						radio.setAttribute("type", "radio");
+						radio.setAttribute("name", "q_" + i + "_a");
+						radio.setAttribute("value", answers[i][j]["trait"]);
+						question_tab.appendChild(radio);
+						question_tab.appendChild(ans_content);
+						question_tab.innerHTML += "<br>";
+					}
+
+					question_step = document.createElement("DIV");
+					question_tab.setAttribute("class", "tab");
+					question_step.setAttribute("class", "step");
+					document.getElementById("total_pages").appendChild(question_tab);
+					document.getElementById("total_steps").appendChild(question_step);
+				}
+
+			}
+
+			function createQuestionPages(currentTab, questions, answers) {
+				var total_pages = document.createElement("DIV");
+				var total_steps = document.createElement("DIV");
+				total_pages.setAttribute("id", "total_pages");
+				total_steps.setAttribute("id", "total_steps");
+				document.getElementById("Quiz_Pages").appendChild(total_pages);
+				document.getElementById("Quiz_Page_Steps").appendChild(total_steps);
+				createQuestion();
+			}
 
 
-	<input type="submit" value="Submit Quiz" />
+			
 
-	</fieldset>
+
+	</script>
 </center>
-</form>
 </body>
 
 </html>
