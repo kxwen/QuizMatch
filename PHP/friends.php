fd026813b70b3e6f28a56f08c132fadfef389f9a
ZatchZ <43761976+ZatchZ@users.noreply.github.com>
2018-12-02 20:19:22 -0800

    Added spacing

--- PHP/friends.php
+++ PHP/friends.php
@@ -17,7 +17,6 @@ $relationships = json_encode($total);
 		<link href= "stupid.css" type = "text/css" rel = "stylesheet"/>
 	</head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<style>
 		div.topBarLayout
@@ -51,7 +50,7 @@ $relationships = json_encode($total);
 		<center>
 			<div class = "bodyLayout">
 				<div class="wrapper">
-					<h2>My Friends</h2>
+					<h2>My Friends</h2><br>
 				</div>
 			</div>
 			<div class="contentRoundBorders">
